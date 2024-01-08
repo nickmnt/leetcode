db19c457fbc261fe17526822d40c968dedce0249
Nick <mtaghavinima@gmail.com>
2024-01-08 11:39:50 -0500

    Lowest common ancestor

---
+++ problems/169_extra/lowest_common_ancestor/solution.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        best = None, -1
+
+        def traverse(r, depth):
+            nonlocal p,q, best
+
+            if not r:
+                return False, False
+
+            p_seen, q_seen = r.val == p.val, r.val == q.val
+
+            for child in [r.left, r.right]:
+                p_seen_next, q_seen_next = traverse(child, depth)
+                p_seen |= p_seen_next
+                q_seen |= q_seen_next
+
+            best_val, best_d = best
+            if depth > best_d and p_seen and q_seen:
+                best = (r, depth)
+
+            return p_seen, q_seen
+
+        traverse(root, 0)
+        return best[0]
+        
